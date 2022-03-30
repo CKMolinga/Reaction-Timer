79f238ff0ff0812225e8fff59ba02ac46e5ece79
CKMolinga <ck.molinga@gmail.com>
2022-03-30 18:12:35 +0200

    added timer

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="block" v-if="showBlock">Click me</div>
+  <div class="block" v-if="showBlock" @click="stopTimer">Click me</div>
 </template>
 
 <script>
@@ -8,14 +8,28 @@ props: ['delay'],
 data() {
   return {
     showBlock: false,
+    timer: null,
+    reactionTime: 0
   }
 },
 mounted () {
   setTimeout(() => {
     this.showBlock = true
+    this.startTimer()
     console.log(this.delay)
   }, this.delay)
 },
+methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10
+      }, 10)
+    }, 
+    stopTimer() {
+      clearInterval(this.timer)
+      console.log(this.reactionTime)
+    }
+}
 }
 </script>
 
