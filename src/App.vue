3a01bb347dfd6fbd99562a26e3f65275d32e96af
CKMolinga <ck.molinga@gmail.com>
2022-03-30 17:27:06 +0200

    First commit: starting a new game

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1>Ninja Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" ></Block>
+</template>
+
+<script>
+import Block from './components/Block.vue'
+
+export default {
+  name: 'App',
+  components: {Block},
+  data () {
+    return {
+      isPlaying: false,
+      delay: null,
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlaying = true
+    }
+  }
+}
+</script>
+
+<style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #444;
+    margin-top: 60px;
+  }
+</style>
---
+++ src/components/Block.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="block">Click me</div>
+</template>
+
+<script>
+export default {
+props: ['delay'],
+}
+</script>
+
+<style>
+.block {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  width: 400px;
+  text-align: center;
+  background: #0faf87;
+  color: #fff;
+  margin: 40px auto;
+  padding: 100px 0;
+  border-radius: 20px;
+  cursor: pointer;
+}
+
+</style>
